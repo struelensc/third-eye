1f78aae7bfcf4967a3cf7071bc1d527ddafc3d48
Courtney Struelens <courtneystruelens@hotmail.com>
2022-11-18 22:44:01 -0800

    removed commented out old code

--- server/server.js
+++ server/server.js
@@ -1,27 +1,3 @@
-// const { ApolloServer } = require("apollo-server");
-// const mongoose = require("mongoose");
-
-// const typeDefs = require("./graphql/typeDefs");
-// const resolvers = require("./graphql/resolvers");
-
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-// });
-
-// mongoose
-//   .connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/third-eye", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     console.log("MonogDB Connection Successful");
-//     return server.listen({ port: 5000 });
-//   })
-//   .then((res) => {
-//     console.log(`Server running at ${res.url}`);
-//   });
-
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
