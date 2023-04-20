887b453488843ac6f14bb34c662dc3049fd3845a
Courtney Struelens <courtneystruelens@hotmail.com>
2023-04-19 21:41:13 -0700

    set up apollo on front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,37 @@
 import React from "react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
 import Landing from "./components/Landing";
 
-const App = () => <Landing />;
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <Landing />
+  </ApolloProvider>
+);
 
 export default App;
